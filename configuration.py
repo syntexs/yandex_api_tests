67fd17b40ace0eb269862ac64127c1f15ebb0df0
syntexs <antystyle@yandex.ru>
2024-02-11 19:33:17 +0500

    firstName api tests

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://58b2bc0b-504e-4f5f-a903-2f412869ab75.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
